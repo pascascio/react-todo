77f444a4523b8cd289f555002c064aa78ace4e48
Kenya <pascasciokenya97@gmail.com>
2024-12-09 11:00:38 -0500

    refactored app.jsx to mimic async data fetching for todolist, add loading state with conditional loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,41 @@ import './App.css'
 import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = React.useState(
-    JSON.parse(localStorage.getItem('savedTodoList')));
-
-  React.useEffect(() => {
-  localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-}, [todoList]);
-
-return[todoList, setTodoList];
-  
-}
-
-
 function App() {
 
-  const [todoList, setTodoList] = useSemiPersistentState();
- 
+  
+   
+  const [todoList, setTodoList] = React.useState([ ]);
+  const[isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')), 
+        }
+      })
+      }, 2000)
+    }).then(result => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    }).catch(error => {
+      console.error('Error:', error)
+    })
+
+    
+  }, []);
+
+
+  React.useEffect(() => {
+    if(!isLoading) {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));}
+  }, [todoList]);
+
+
+
+
+
   function addTodo(newTodo){
     setTodoList([...todoList, newTodo])
   }
@@ -32,9 +50,10 @@ function App() {
 
   return(
   <>
+
    <h1>Todo List</h1>
    <AddTodoForm onAddTodo = {addTodo}/>
-   <TodoList todoList = {todoList} onRemoveTodo = {removeTodo}/>
+   {isLoading ? <p>Loading....</p> :  <TodoList todoList = {todoList} onRemoveTodo = {removeTodo}/>}
   </>
   )
 }
