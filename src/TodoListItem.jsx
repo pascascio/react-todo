4052f57377d4f2d1b92852c99572d25394d6f840
Kenya <pascasciokenya97@gmail.com>
2025-01-28 22:46:28 -0500

    use css modules to style todo app

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,6 +1,9 @@
+
+import styles from './TodoListItem.module.css'
+
 function TodoListItem({todo, onRemoveTodo, id}){
 return(
-    <li> {todo} <button type ="button" onClick = {() => onRemoveTodo(id)}> Remove </button></li>
+    <li className = {styles.ListItem}> {todo} <button type ="button" onClick = {() => onRemoveTodo(id)}> Remove </button></li>
 )
 }
 
